159b78c4a2e9316ea639df65d6e5814994cb4e2d
alishehata <62440590+alishehata@users.noreply.github.com>
2020-03-20 23:23:29 +0200

    Add files via upload

---
+++ ExpressionSpecifications.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Models
+{
+    /// <summary>
+    /// Column Expression Specifications Used to store Expression Specifications
+    /// </summary>
+    public class ColumnFilterExpressions
+    {
+        /// <summary> Column value to filter with </summary>
+        public string FirstValue { get; set; }
+
+        /// <summary> Column value to filter with </summary>
+        public string SecondValue { get; set; }
+
+        /// <summary> when using selected Set </summary>
+        public IList<string> ElementSet { get; set; } 
+
+        /// <summary> comparision Operation used in filter </summary>
+        public string Operand { get; set; }
+    }
+}
