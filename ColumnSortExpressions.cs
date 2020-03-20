59542411f5b76a405f1a8defa725b4dc092100de
alishehata <62440590+alishehata@users.noreply.github.com>
2020-03-20 23:15:25 +0200

    Create ColumnSortExpressions.cs

---
+++ ColumnSortExpressions.cs
@@ -0,0 +1,13 @@
+using Enums;
+
+namespace Models
+{
+    /// <summary>
+    /// Sorting columns options
+    /// </summary>
+    public class ColumnSortExpressions 
+    { 
+        public string ColumnName { get; set; }
+        public string OrderType { get; set; } 
+    }
+}
