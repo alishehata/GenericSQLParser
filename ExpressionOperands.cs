7ae36b43c38bcb4bc1bea7b77aa2ec2c70ce6cb2
alishehata <62440590+alishehata@users.noreply.github.com>
2020-03-20 23:16:32 +0200

    Create ExpressionOperands.cs

---
+++ ExpressionOperands.cs
@@ -0,0 +1,13 @@
+namespace Models
+{
+    public class ExpressionOperands
+    {
+        public const string In = "in";
+        public const string Between = "between";
+        public const string Matches = "matches";
+        public const string StartsWith = "starts-with";
+        public const string EndsWith = "ends-with";
+        public const string Contains = "contains";
+        public const string NotContains = "not-contains";
+    }
+}
